624685d9fdf1cdc977fc181179fc6ec0d20c174b
Launcher-minecraft <94561445+Launcher-minecraft@users.noreply.github.com>
2022-04-30 15:05:53 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,60 @@
+const Discord = require("discord.js");
+const client = new Discord.Client({ intents: 32767, partials: ["CHANNEL", "GUILD_MEMBER", "MESSAGE", "REACTION", "USER"] });
+const fs = require("fs");
+const config = require("./config.json");
+client.config = config;
+
+const { GiveawaysManager } = require("discord-giveaways");
+client.giveawaysManager = new GiveawaysManager(client, {
+  storage: "./storage/giveaways.json",
+  default: {
+    botsCanWin: false,
+    embedColor: "#2F3136",
+    reaction: "🎉",
+    lastChance: {
+      enabled: true,
+      content: `🛑 **Last chance to enter** 🛑`,
+      threshold: 5000,
+      embedColor: '#FF0000'
+    }
+  }
+});
+
+fs.readdir("./events/discord", (_err, files) => {
+  files.forEach(file => {
+    if (!file.endsWith(".js")) return;
+    const event = require(`./events/discord/${file}`);
+    let eventName = file.split(".")[0];
+    console.log(`[Event]   ✅  Loaded: ${eventName}`);
+    client.on(eventName, event.bind(null, client));
+    delete require.cache[require.resolve(`./events/discord/${file}`)];
+  });
+});
+
+fs.readdir("./events/giveaways", (_err, files) => {
+  files.forEach((file) => {
+    if (!file.endsWith(".js")) return;
+    const event = require(`./events/giveaways/${file}`);
+    let eventName = file.split(".")[0];
+    console.log(`[Event]   🎉 Loaded: ${eventName}`);
+    client.giveawaysManager.on(eventName, (...file) => event.execute(...file, client)), delete require.cache[require.resolve(`./events/giveaways/${file}`)];
+  })
+})
+
+
+client.interactions = new Discord.Collection();
+client.register_arr = []
+fs.readdir("./slash/", (_err, files) => {
+  files.forEach(file => {
+    if (!file.endsWith(".js")) return;
+    let props = require(`./slash/${file}`);
+    let commandName = file.split(".")[0];
+    client.interactions.set(commandName, {
+      name: commandName,
+      ...props
+    });
+    client.register_arr.push(props)
+  });
+});
+
+client.login(config.token);
